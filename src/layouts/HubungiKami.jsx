7348ab80f1df0245e47ea103f2088fc0037cd5b5
Fiqih Ref <fiqihref15@gmail.com>
2024-08-21 15:30:22 +0700

    modify footer and contact us

--- src/layouts/HubungiKami.jsx
+++ src/layouts/HubungiKami.jsx
@@ -6,23 +6,23 @@ const HubungiKami = () => {
           <img className="w-1/2 md:w-2/3" src="/images/question.svg" alt="question" />
         </div>
         <div className="md:w-2/3 self-center">
-          <h4 className="text-2xl md:text-4xl font-bold text-center text-primary">
+          <h4 className="text-4xl lg:text-5xl  font-bold text-center text-primary">
             Masih bingung? Mau tanya-tanya?
           </h4>
-          <p className="text-center text-gray-800 text-sm md:text-md mt-2">
+          <p className="text-center text-gray-800 text-md md:text-lg mt-2">
             Silahkan hubungi kami untuk informasi lebih lanjut
           </p>
           <div className="flex justify-center pt-4 gap-3">
             <a target="_blank" href="https://wa.me/6281234567890">
               <img
-                className="h-12 md:h-16 filter grayscale hover:filter-none transition duration-300 ease-in-out"
+                className="h-20 md:h-16 filter grayscale hover:filter-none transition duration-300 ease-in-out"
                 src="/icons/whatsapp.svg"
                 alt="whatsapp kami"
               />
             </a>
             <a target="_blank" href="mailto:lpkgokuryeo@gmail.com">
               <img
-                className="h-12 md:h-16 filter grayscale hover:filter-none transition duration-300 ease-in-out"
+                className="h-20 md:h-16 filter grayscale hover:filter-none transition duration-300 ease-in-out"
                 src="/icons/gmail.svg"
                 alt="email kami"
               />
